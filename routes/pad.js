77296b1db5a1ee010bafc49b969d9480b708cb6b
shining <shine1988@126.com>
2016-03-15 10:52:57 +0800

    update

--- routes/pad.js
+++ routes/pad.js
@@ -458,28 +458,35 @@ exports.get_form = function(req, res, next){
 	var values = [school_id, class_id];
 	sql.query(req, res, sql_mapping.get_form, values, next, function(err, ret){
 		var student_obj = [];
-		var content = JSON.parse(ret[0].content);
-		for (var i=0;i<content.length;i++){
-			var name = content[i].name;
-			var num = content[i].num;
-			var sex = content[i].sex;
-			var class_id = content[i].class_id;
-			var id = content[i].id;
-			var score_list = content[i].score_list;	
-			var score_obj = [];
-			for (var u=0;u<score_list.length;u++){
-				var item_id = score_list[u].item_id;
-				var record = score_list[u].record;
-				var score = score_list[u].score;
-				var level = score_list[u].level;
-				score_obj.push({item_id:item_id, record:record, score:score, level:level});
-			}
-			student_obj.push({name : name, num : num, sex : sex, id : id, score_list : score_obj});
-		}	
-		result.header.code = "200";
-		result.header.msg  = "成功";
-		result.data = {content : student_obj, class_id : class_id, teacher : ret[0].submit_teacher, title : ret[0].title, time : ret[0].submit_time};
-		res.json(result);
+		try{
+			var content = JSON.parse(ret[0].content);
+			for (var i=0;i<content.length;i++){
+				var name = content[i].name;
+				var num = content[i].num;
+				var sex = content[i].sex;
+				var class_id = content[i].class_id;
+				var id = content[i].id;
+				var score_list = content[i].score_list;	
+				var score_obj = [];
+				for (var u=0;u<score_list.length;u++){
+					var item_id = score_list[u].item_id;
+					var record = score_list[u].record;
+					var score = score_list[u].score;
+					var level = score_list[u].level;
+					score_obj.push({item_id:item_id, record:record, score:score, level:level});
+				}
+				student_obj.push({name : name, num : num, sex : sex, id : id, score_list : score_obj});
+			}	
+			result.header.code = "200";
+			result.header.msg  = "成功";
+			result.data = {content : student_obj, class_id : class_id, teacher : ret[0].submit_teacher, title : ret[0].title, time : ret[0].submit_time};
+			res.json(result);
+		}catch(err){
+			result.header.code = "200";
+			result.header.msg  = "成功";
+			result.data = {};
+			res.json(result);
+		}
 	});
 };
 
